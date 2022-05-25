25629436631207ee3607854e52884b0231d4cadb
Andreas Tasch <andy.tasch@gmail.com>
2022-05-25 10:40:28 +0200

    Only log when debug log checked.

--- src/Helper/Logger.php
+++ src/Helper/Logger.php
@@ -7,7 +7,7 @@ namespace NoFrixion\WC\Helper;
 class Logger {
 
 	public static function debug($message, $force = false): void {
-		//if ( get_option( 'nofrixion_debug' ) === 'yes' || $force ) {
+		if ( get_option( 'nofrixion_debug' ) === 'yes' || $force ) {
 			// Convert message to string
 			if ( ! is_string( $message ) ) {
 				$message = wc_print_r( $message, true );
@@ -16,7 +16,7 @@ class Logger {
 			$logger = new \WC_Logger();
 			$context = array( 'source' => NOFRIXION_PLUGIN_ID );
 			$logger->debug( $message, $context );
-		//}
+		}
 	}
 
 	public static function getLogFileUrl(): string {
