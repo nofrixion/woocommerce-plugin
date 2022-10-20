e19c517bcac3980bc276ba333cd0605a26d5f60b
Aaron Clauson <aaron@nofrixion.com>
2022-10-20 14:59:50 +0100

    Bumped version to v1.1.21.

--- nofrixion-for-woocommerce.php
+++ nofrixion-for-woocommerce.php
@@ -7,7 +7,7 @@
  * Author URI:      https://nofrixion.com
  * Text Domain:     nofrixion-for-woocommerce
  * Domain Path:     /languages
- * Version:         1.1.20
+ * Version:         1.1.21
  * Requires PHP:    7.4
  * Tested up to:    6.0
  * Requires at least: 5.2
@@ -20,7 +20,7 @@ use NoFrixion\WC\Helper\TokenManager;
 
 defined( 'ABSPATH' ) || exit();
 
-define( 'NOFRIXION_VERSION', '1.1.20' );
+define( 'NOFRIXION_VERSION', '1.1.21' );
 define( 'NOFRIXION_PLUGIN_FILE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'NOFRIXION_PLUGIN_URL', plugin_dir_url(__FILE__ ) );
 define( 'NOFRIXION_PLUGIN_ID', 'nofrixion-for-woocommerce' );
